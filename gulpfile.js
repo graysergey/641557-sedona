201e7100d6687d33c1d0781a2168f7c24dde3558
GraySergey <xgortx@gmail.com>
2018-10-27 10:39:39 +0300

    Работа над ошибками автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task("images", function () {
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("webp", function() {
